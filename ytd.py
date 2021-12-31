ab45e79dc89e92e17084382b9f29d57e4e8b7e10
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2021-12-30 20:33:11 -0500

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -4,23 +4,20 @@ import os
 
 '''Python 3 Script'''
 
-src = os.getcwd()
 
 def youtubedownload():
-	url = input('Please enter the URL:  ')
-	print('Downloading... Please Wait... ')
-	ytd = YouTube(url)
-	stream = ytd.streams.get_by_itag(22)
-	stream.download()
-	os.system('cls' if os.name == 'nt' else 'clear')
-	print("Downloaded " + url)
-	for files in os.listdir(src):
-		# print(files[-4:])
-		if files[-4:] == ".mp4":
-			print("Downloads Complete: {0}".format(files))
+    url = input('Please enter the URL:  ')
+    print('Downloading... Please Wait... ')
+    ytd = YouTube(url)
+    stream = ytd.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first()
+    stream.download(timeout=100000)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(ytd)
+
 
 youtubedownload()
 input('Enter To Continue... ')
 
 while True:
-	youtubedownload()
+    youtubedownload()
